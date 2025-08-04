0737b9c28da10d7608fc145915c2f20baec5d343
Cássio Gadêlha <61876421+cassiogadelha@users.noreply.github.com>
2025-08-04 17:10:19 -0300

    WireMock working

--- booking-api/src/test/java/verso/caixa/client/WireMockVehicleAPI.java
+++ booking-api/src/test/java/verso/caixa/client/WireMockVehicleAPI.java
@@ -17,8 +17,24 @@ public class WireMockVehicleAPI implements QuarkusTestResourceLifecycleManager {
         wireMockServer.start();
 
         // Stub do veículo
-        wireMockServer.stubFor(get(urlPathMatching("/api/v1/vehicles/.*"))
+        wireMockServer.stubFor(get(urlPathMatching("/api/v1/vehicles/([a-zA-Z0-9\\-]+)"))
                 .willReturn(okJson("{ \"status\": \"AVAILABLE\" }")));
+
+        /*
+        intercepta qualquer requisição HTTP GET para a rota /api/v1/vehicles/{algum-UUID} e devolver uma resposta
+        estática com JSON dizendo que o veículo está disponível.
+
+        get(...)
+        Indica que o stub é para requisições HTTP GET
+
+        urlPathMatching(...)
+        expressão regular que combina qualquer URL que comece com /api/v1/vehicles/ seguido por um UUID.
+        A regex: ([a-zA-Z0-9\\-]+)
+
+        .willReturn(...)
+        Define o que WireMock deve responder quando essa requisição for feita
+
+         */
 
         return Map.of(
                 "quarkus.rest-client.vehicle-api.url", wireMockServer.baseUrl() + "/api/v1"
